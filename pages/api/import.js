b0d372896f856a0cd67c3c43d81519442e2f89d2
Anirban Chakraborty <anchakrabort@glanbia.ie>
2022-11-11 02:48:47 +0000

    feature/10

--- pages/api/import.js
+++ pages/api/import.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { parseAsFile, parseAsStringForAPI } from '../../lib/utils/tsv2json';
+import { parseAsFile, parseAsString } from '../../lib/utils/tsv2json';
 export default async function handler(req, res) {
   if (req.method == 'GET') {
     try {
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
   } else if (req.method == 'POST') {
     try {
-      const result = await parseAsStringForAPI(req.body?.csv);
+      const result = await parseAsString(req.body?.csv);
       result
         ? res.status(200).json(result)
         : res.status(500).json('Import Failed');
