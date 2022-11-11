91be1ed1af08d4e0fd84e2ff70920be4d15207b9
Anirban Chakraborty <anchakrabort@glanbia.ie>
2022-11-11 01:19:58 +0000

    feature/2

---
+++ components/Form/index.js
@@ -0,0 +1,62 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { parseAsString } from "../../lib/utils/tsv2json";
+import style from "./Form.module.css";
+export default function (props) {
+  return (
+    <>
+      <Formik
+        enableReinitialize
+        initialValues={{ content: props.content }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.content) {
+            errors.content = "This is a Required field";
+          }
+          return errors;
+        }}
+        //separte func
+        onSubmit={async (values) => {
+          let resp = {};
+          let error = {};
+          try {
+            const response = await parseAsString(values);
+            resp = response.json;
+            error = response.error;
+          } catch (e) {
+            console.log(e.message);
+            // error = e;
+          }
+          props.onSubmitForm(resp, error, values.content);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className={style.container}>
+            <label className={style.label} htmlFor="content">
+              TSV Content
+            </label>
+            <Field
+              className={`${style.input}`}
+              component="textarea"
+              rows="30"
+              id="content"
+              name="content"
+              cols="50"
+            />
+            <ErrorMessage
+              className={style.error}
+              name="content"
+              component="div"
+            />
+            <button
+              className={style.button}
+              type="submit"
+              onClick={isSubmitting ? () => false : null}
+            >
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+}
