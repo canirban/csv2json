52deb5d416a58c5be7c0f9c468db40c406c3f643
Anirban Chakraborty <anchakrabort@glanbia.ie>
2022-11-11 01:30:28 +0000

    feature/3

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ yarn dev
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 To test the api access [http://localhost:3000/api/import](http://localhost:3000/api/import)
-If its a GET call then the file data.csv will be imported and JSON be displayed on the screen
-If its a POST request you can put your own payload to test the api make sure to add key as csv to the body and value as the csv text.
+Must be a POST request you can put your own payload to test the api make sure to add key as csv to the body and value as the csv text.
+
 # csv2json
--- pages/api/import.js
+++ pages/api/import.js
@@ -1,17 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { parseAsFile, parseAsStringForAPI } from "../../lib/utils/tsv2json";
+import { parseAsStringForAPI } from "../../lib/utils/tsv2json";
 export default async function handler(req, res) {
-  if (req.method == "GET") {
-    try {
-      const result = await parseAsFile("data.csv");
-      //localStorage?.setItem(page, JSON.stringify({ resp, error, content }));
-      result
-        ? res.status(200).json(result)
-        : res.status(500).json("Import Failed");
-    } catch (e) {
-      res.status(500).json(`Import failed. Reason :${e.message}`);
-    }
-  } else if (req.method == "POST") {
+  if (req.method == "POST") {
     try {
       const result = await parseAsStringForAPI(req.body?.csv);
       result
